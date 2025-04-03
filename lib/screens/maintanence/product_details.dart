5b3a0f58a82c3be7cb5e35c1b211733c2727dae1
Aljoboyer <tanveeazmain@gmail.com>
2025-04-03 12:29:15 +0600

    details page design started

--- lib/screens/maintanence/product_details.dart
+++ lib/screens/maintanence/product_details.dart
@@ -13,26 +13,54 @@ class ProductDetails extends StatefulWidget {
 class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    
     return Scaffold(
         appBar: AppBar(
-           elevation: 1,
+        title: Text('Details', style: TextStyle(fontWeight: FontWeight.bold),),
+        elevation: 1, 
         shadowColor: Colors.grey,
-    leading: IconButton(
-      icon: Icon(Icons.arrow_back), // Back arrow icon
-      onPressed: () {
-         context.go('/tabs/0'); //us screen
-      },
-        ),
-      ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back), // Back arrow icon
+          onPressed: () {
+            context.go('/tabs/0'); //us screen
+          },
+            ),
+          ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            Text('Hello'),
             CustomButton(btn_label: 'Sell Now', onPressed: () {
               context.go('/tabs/2'); 
+            }, width: screenWidth * 10,),
 
-            },)
+            SizedBox(height: 20,),
+            
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Ceramic Tiles', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF044B91)),),
+            ),
+
+            SizedBox(height: 8,),
+
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Size: 300x300 mm (12x12 inches)', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Color.fromARGB(255, 3, 3, 3)),),
+            ),
+
+            SizedBox(height: 10,),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Total Sold: 345345 Pieces', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: Color.fromARGB(255, 0, 92, 197)),),
+                Text('Available: 545345 Pieces', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: Color.fromARGB(255, 0, 92, 197)),),
+              ],
+            ),
+
+
           ],
         ),
       ),
--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final String btn_label;
   final VoidCallback onPressed;
+  final double? width;
 
   const CustomButton({
     super.key,
     required this.btn_label,
     required this.onPressed,
+    this.width
   });
 
   @override
   Widget build(BuildContext context) {
     return  SizedBox(
                         height: 50,
-                        width: 300,
+                        width: width ?? 300,
                         child: ElevatedButton(onPressed: onPressed,
                         style: ElevatedButton.styleFrom(
                             backgroundColor: Color(0xFF044B91),
